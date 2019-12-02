7fde66842d654dc0ebd6802ab2a447c377db532e
kangdian <oyomcp@gmail.com>
2019-12-02 20:04:52 +0700

    init project bootcamp

---
+++ src/test/java/com/xsis/bootcamp/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsis.bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
