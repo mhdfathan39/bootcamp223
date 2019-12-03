f6a3a018dd308b212a9ee44c152b4193e3840f5a
kangdian <oyomcp@gmail.com>
2019-12-03 07:28:36 +0700

    templating done

--- src/main/java/com/xsis/bootcamp/controller/HomeController.java
+++ src/main/java/com/xsis/bootcamp/controller/HomeController.java
@@ -7,7 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/home")
-    public String getHome() {
+    public String viewHome() {
         return "home";
+    }
+
+    @GetMapping("/index")
+    public String viewWelcome() {
+        return "index";
+    }
+
+    @GetMapping("/employee")
+    public String viewEmployee() {
+        return "pages/employee";
     }
 }
